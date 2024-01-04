8be1f0017303400873b4c21a80ff6b4a4b16f426
Wolvie <mishravatsalya74@gmail.com>
2024-01-04 23:05:20 +0530

    added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot GenAI Project',
+    version= '0.0.0',
+    author= 'Vatsalya Kumar Mishra',
+    author_email= 'mishravatsalya74@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
